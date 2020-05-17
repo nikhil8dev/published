bc72f4a9187b2fed865a0f2ed10ff20923607127
nikhil8dev <nikhil7797patil@gmail.com>
2020-05-18 01:41:11 +0530

    Reactive form For contact

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
-
+  contactForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.createContactForm();
+  }
   ngOnInit(): void {
+
   }
 
+  createContactForm(){
+    this.contactForm = this.formBuilder.group({
+      fullName: [''],
+      email: [''],
+      message: [''],
+      address: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zip: ['']
+      }),
+    });
+  }
+
+  onSubmit() {
+      console.log('Your form data : ', this.contactForm.value );
+  }
 }
+
+
